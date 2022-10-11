c1099378e2e34549ee8993c369075886df7c29ab
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-10-11 15:38:46 -0400

    update api url

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const getWeather = async (city) => {
     try {
-        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=1de2fe9bfb96c5bb2212c3fe482db260`;
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1de2fe9bfb96c5bb2212c3fe482db260`;
         const response = await fetch(url);
         const data = response.json();
         console.log(data);        
     } catch (error) {
         console.error(error);
     }
-    
+
 }
+
